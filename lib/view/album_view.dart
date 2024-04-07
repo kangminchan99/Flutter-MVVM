de0736b3a7c3288c1c63d519846ab6e3666ce64b
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-04-07 21:14:56 +0900

    Consumer를 통해 뷰에서 띄워준다.

--- lib/view/album_view.dart
+++ lib/view/album_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm/model/album.dart';
+import 'package:flutter_mvvm/viewModel/albumViewModel.dart';
+import 'package:provider/provider.dart';
 
 class AlbumView extends StatefulWidget {
   const AlbumView({super.key});
@@ -8,8 +11,21 @@ class AlbumView extends StatefulWidget {
 }
 
 class _AlbumViewState extends State<AlbumView> {
+  late List<Album> albumList;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('MVVM')),
+      body: Consumer<AlbumViewModel>(builder: (context, provider, child) {
+        albumList = provider.albumList;
+        return ListView.builder(itemBuilder: (context, index) {
+          return Container(
+            padding: EdgeInsets.all(15),
+            child: Text('${albumList[index].id}: ${albumList[index].title}'),
+          );
+        });
+      }),
+    );
   }
 }
